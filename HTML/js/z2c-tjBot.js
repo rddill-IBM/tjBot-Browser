2926bdd24ae2c26c1ca2b671fa142111cb7f47ab
Robert Dill <rddill@us.ibm.com>
2018-02-20 15:33:29 -0500

    attempting video with raspivid

--- HTML/js/z2c-tjBot.js
+++ HTML/js/z2c-tjBot.js
@@ -51,7 +51,10 @@ function initTJBot()
     canvasFrame = $('#videoInput');
     console.log('initiating post request to /tjBot/getWebCam');
     wsSocket = new WebSocket(socketAddr);
-    wsSocket.onmessage = function (data) {console.log("message from server", data);}
+    wsSocket.onmessage = function (data) 
+    {
+        console.log("message from server", data.data);
+    }
     wsSocket.onopen = function () {wsSocket.send('connected to client');};
     wsSocket.onerror = function (error) {console.log('WebSocket error on wsSocket: ' + error);};
 
